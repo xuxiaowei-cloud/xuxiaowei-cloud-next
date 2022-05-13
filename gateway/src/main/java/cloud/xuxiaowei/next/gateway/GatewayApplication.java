50136938bca16601b2261d97653b14bbd89aae4b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-13 17:21:28 +0800

    add gateway

---
+++ gateway/src/main/java/cloud/xuxiaowei/next/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package cloud.xuxiaowei.next.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
