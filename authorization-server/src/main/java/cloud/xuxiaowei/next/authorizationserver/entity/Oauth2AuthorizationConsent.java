e3eef3a29a5e97ece85d34ea31d914d348640e2d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-19 00:26:57 +0800

    📝:修正注释

--- authorization-server/src/main/java/cloud/xuxiaowei/next/authorizationserver/entity/Oauth2AuthorizationConsent.java
+++ authorization-server/src/main/java/cloud/xuxiaowei/next/authorizationserver/entity/Oauth2AuthorizationConsent.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 
 /**
  * <p>
- * 权限表。	原表结构：oauth2-authorization-server-*.*.*.jar!/org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql	原表结构：https://github.com/spring-projects/spring-authorization-server/blob/main/oauth2-authorization-server/src/main/resources/org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql	GitCode 镜像仓库：https://gitcode.net/mirrors/spring-projects/spring-authorization-server/blob/main/oauth2-authorization-server/src/main/resources/org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql
+ * 授权同意书表。	原表结构：oauth2-authorization-server-*.*.*.jar!/org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql	原表结构：https://github.com/spring-projects/spring-authorization-server/blob/main/oauth2-authorization-server/src/main/resources/org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql	GitCode 镜像仓库：https://gitcode.net/mirrors/spring-projects/spring-authorization-server/blob/main/oauth2-authorization-server/src/main/resources/org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql
  * </p>
  *
  * @author xuxiaowei
