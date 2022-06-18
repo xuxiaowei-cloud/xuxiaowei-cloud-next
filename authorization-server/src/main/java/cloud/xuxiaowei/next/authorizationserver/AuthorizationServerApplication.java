de6be7297885c9d2a8ccd30eb2c6d7f0cd7b05e6
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-18 19:52:50 +0800

    🎨:format

--- authorization-server/src/main/java/cloud/xuxiaowei/next/authorizationserver/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/cloud/xuxiaowei/next/authorizationserver/AuthorizationServerApplication.java
@@ -12,8 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthorizationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
 
 }
