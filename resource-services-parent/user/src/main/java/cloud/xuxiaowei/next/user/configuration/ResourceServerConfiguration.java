f4425d034c02affe6fb178297764bb66acb46b4c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-21 18:49:29 +0800

    ✨:禁用 form 登录

--- resource-services-parent/user/src/main/java/cloud/xuxiaowei/next/user/configuration/ResourceServerConfiguration.java
+++ resource-services-parent/user/src/main/java/cloud/xuxiaowei/next/user/configuration/ResourceServerConfiguration.java
@@ -1,12 +1,9 @@
 package cloud.xuxiaowei.next.user.configuration;
 
 import cloud.xuxiaowei.next.core.properties.JwkKeyProperties;
-import cloud.xuxiaowei.next.utils.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -41,19 +38,21 @@ public class ResourceServerConfiguration {
      * @see <a href="https://docs.spring.io/spring-security/reference/servlet/authentication/index.html">用于身份验证</a> 的 Spring Security 过滤器链。
      */
     @Bean
-    @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        // 表单登录处理从授权服务器过滤器链到登录页面的重定向
+        // 路径权限控制
         http.authorizeHttpRequests((authorize) -> {
             authorize
-                    // 端点放行
-                    .antMatchers("/" + Constant.ACTUATOR + "/**").permitAll()
-                    // 注销登录
+                    // 放行端点
+                    .antMatchers("/actuator/**").permitAll()
+                    // 注销登录放行
                     .antMatchers("/signout").permitAll()
                     // 其他路径均需要授权
                     .anyRequest().authenticated();
-        }).formLogin(Customizer.withDefaults());
+        });
+
+        // 禁用 form 登录
+        http.formLogin().disable();
 
         http.oauth2ResourceServer().jwt(oauth2ResourceServer -> {
             RSAPublicKey rsaPublicKey = jwkKeyProperties.rsaPublicKey();
