babd3c5bcbc0d172c9ad64af1decdc19b1c36726
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-22 20:40:28 +0800

    🎨:代码格式规范 spring-javaformat

--- passport/src/main/java/cloud/xuxiaowei/next/passport/entity/UsersLogin.java
+++ passport/src/main/java/cloud/xuxiaowei/next/passport/entity/UsersLogin.java
@@ -18,121 +18,120 @@ import java.time.LocalDateTime;
 @TableName("users_login")
 public class UsersLogin implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * 用户登录主键，自增
-     */
-    @TableId(value = "users_login_id", type = IdType.AUTO)
-    private Long usersLoginId;
+	/**
+	 * 用户登录主键，自增
+	 */
+	@TableId(value = "users_login_id", type = IdType.AUTO)
+	private Long usersLoginId;
 
-    /**
-     * 用户名
-     */
-    private String username;
+	/**
+	 * 用户名
+	 */
+	private String username;
 
-    /**
-     * 是否成功，不为空
-     */
-    private Boolean success;
+	/**
+	 * 是否成功，不为空
+	 */
+	private Boolean success;
 
-    /**
-     * 请求方法
-     */
-    private String method;
+	/**
+	 * 请求方法
+	 */
+	private String method;
 
-    /**
-     * 参数
-     */
-    private String queryString;
+	/**
+	 * 参数
+	 */
+	private String queryString;
 
-    /**
-     * 请求头，json
-     */
-    private String headersMap;
+	/**
+	 * 请求头，json
+	 */
+	private String headersMap;
 
-    /**
-     * 标识
-     */
-    private String userAgent;
+	/**
+	 * 标识
+	 */
+	private String userAgent;
 
-    /**
-     * 请求ID，不为空
-     */
-    private String requestId;
+	/**
+	 * 请求ID，不为空
+	 */
+	private String requestId;
 
-    /**
-     * Session ID
-     */
-    private String sessionId;
+	/**
+	 * Session ID
+	 */
+	private String sessionId;
 
-    /**
-     * IP，不为空
-     */
-    private String ip;
+	/**
+	 * IP，不为空
+	 */
+	private String ip;
 
-    /**
-     * 异常
-     */
-    private String exception;
+	/**
+	 * 异常
+	 */
+	private String exception;
 
-    /**
-     * browscap 是否处理
-     */
-    private Boolean browscap;
+	/**
+	 * browscap 是否处理
+	 */
+	private Boolean browscap;
 
-    /**
-     * 浏览器值（例如 Chrome）
-     */
-    private String browser;
+	/**
+	 * 浏览器值（例如 Chrome）
+	 */
+	private String browser;
 
-    /**
-     * 浏览器类型（例如 Browser 或 Application）
-     */
-    private String browserType;
+	/**
+	 * 浏览器类型（例如 Browser 或 Application）
+	 */
+	private String browserType;
 
-    /**
-     * 浏览器的主要版本（例如，Chrome 为 55）
-     */
-    private String browserMajorVersion;
+	/**
+	 * 浏览器的主要版本（例如，Chrome 为 55）
+	 */
+	private String browserMajorVersion;
 
-    /**
-     * 平台名称（例如 Android、iOS、Win7、Win10）
-     */
-    private String platform;
+	/**
+	 * 平台名称（例如 Android、iOS、Win7、Win10）
+	 */
+	private String platform;
 
-    /**
-     * 平台版本（例如 4.2、10，取决于平台是什么）
-     */
-    private String platformVersion;
+	/**
+	 * 平台版本（例如 4.2、10，取决于平台是什么）
+	 */
+	private String platformVersion;
 
-    /**
-     * 设备类型（例如手机、台式机、平板电脑、控制台、电视设备）
-     */
-    private String deviceType;
+	/**
+	 * 设备类型（例如手机、台式机、平板电脑、控制台、电视设备）
+	 */
+	private String deviceType;
 
-    /**
-     * 备注
-     */
-    private String remark;
+	/**
+	 * 备注
+	 */
+	private String remark;
 
-    /**
-     * 创建时间，不为空，数据库自动生成
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private LocalDateTime createDate;
+	/**
+	 * 创建时间，不为空，数据库自动生成
+	 */
+	@TableField(fill = FieldFill.INSERT)
+	private LocalDateTime createDate;
 
-    /**
-     * 更新时间，未更新时为空
-     */
-    @TableField(fill = FieldFill.UPDATE)
-    private LocalDateTime updateDate;
+	/**
+	 * 更新时间，未更新时为空
+	 */
+	@TableField(fill = FieldFill.UPDATE)
+	private LocalDateTime updateDate;
 
-    /**
-     * 逻辑删除，0 未删除，1 删除，MySQL 默认值 0，不为 NULL，注解@TableLogic。
-     */
-    @TableLogic
-    private Boolean deleted;
-
+	/**
+	 * 逻辑删除，0 未删除，1 删除，MySQL 默认值 0，不为 NULL，注解@TableLogic。
+	 */
+	@TableLogic
+	private Boolean deleted;
 
 }
--- utils/src/main/java/cloud/xuxiaowei/next/utils/RequestUtils.java
+++ utils/src/main/java/cloud/xuxiaowei/next/utils/RequestUtils.java
@@ -22,86 +22,83 @@ import java.util.*;
 @Slf4j
 public class RequestUtils {
 
-    /**
-     * 获取 Headers Map
-     *
-     * @param request 请求
-     * @return 返回 Headers Map
-     */
-    public static Map<String, List<String>> getHeadersMap(@NonNull HttpServletRequest request) {
-        Map<String, List<String>> map = new HashMap<>(8);
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            Enumeration<String> headerValues = request.getHeaders(headerName);
-            List<String> headerValuesList = Collections.list(headerValues);
-            map.put(headerName, headerValuesList);
-        }
-        return map;
-    }
+	/**
+	 * 获取 Headers Map
+	 * @param request 请求
+	 * @return 返回 Headers Map
+	 */
+	public static Map<String, List<String>> getHeadersMap(@NonNull HttpServletRequest request) {
+		Map<String, List<String>> map = new HashMap<>(8);
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			Enumeration<String> headerValues = request.getHeaders(headerName);
+			List<String> headerValuesList = Collections.list(headerValues);
+			map.put(headerName, headerValuesList);
+		}
+		return map;
+	}
 
-    /**
-     * 获取 Headers JSON
-     *
-     * @param request 请求
-     * @return 返回 Headers JSON
-     */
-    public static String getHeadersJson(@NonNull HttpServletRequest request) {
-        Map<String, List<String>> headersMap = getHeadersMap(request);
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.writeValueAsString(headersMap);
-        } catch (JsonProcessingException e) {
-            log.error("使用jackson将请求头处理为JSON异常：", e);
-        }
-        return null;
-    }
+	/**
+	 * 获取 Headers JSON
+	 * @param request 请求
+	 * @return 返回 Headers JSON
+	 */
+	public static String getHeadersJson(@NonNull HttpServletRequest request) {
+		Map<String, List<String>> headersMap = getHeadersMap(request);
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			return objectMapper.writeValueAsString(headersMap);
+		}
+		catch (JsonProcessingException e) {
+			log.error("使用jackson将请求头处理为JSON异常：", e);
+		}
+		return null;
+	}
 
-    /**
-     * 获取标识
-     *
-     * @param request 请求
-     * @return 返回 标识
-     */
-    public static String getUserAgent(@NonNull HttpServletRequest request) {
-        return request.getHeader(HttpHeaders.USER_AGENT);
-    }
+	/**
+	 * 获取标识
+	 * @param request 请求
+	 * @return 返回 标识
+	 */
+	public static String getUserAgent(@NonNull HttpServletRequest request) {
+		return request.getHeader(HttpHeaders.USER_AGENT);
+	}
 
-    /**
-     * 请求参数流
-     *
-     * @param request 请求
-     * @return 返回 请求参数流
-     * @throws IOException 读取流异常
-     * @see HttpServletRequest#getContentLength()  请关注是否为负数
-     */
-    public static String getInputStream(HttpServletRequest request) throws IOException {
-        ServletInputStream inputStream = request.getInputStream();
-        int contentLength = request.getContentLength();
-        if (contentLength == -1) {
-            // GET 请求时
-            // DELETE、HEAD、OPTIONS 请求未设置流时
-            return null;
-        } else {
-            byte[] bytes = new byte[contentLength];
-            String characterEncoding = request.getCharacterEncoding();
-            inputStream.read(bytes);
-            return new String(bytes, characterEncoding);
-        }
-    }
+	/**
+	 * 请求参数流
+	 * @param request 请求
+	 * @return 返回 请求参数流
+	 * @throws IOException 读取流异常
+	 * @see HttpServletRequest#getContentLength() 请关注是否为负数
+	 */
+	public static String getInputStream(HttpServletRequest request) throws IOException {
+		ServletInputStream inputStream = request.getInputStream();
+		int contentLength = request.getContentLength();
+		if (contentLength == -1) {
+			// GET 请求时
+			// DELETE、HEAD、OPTIONS 请求未设置流时
+			return null;
+		}
+		else {
+			byte[] bytes = new byte[contentLength];
+			String characterEncoding = request.getCharacterEncoding();
+			inputStream.read(bytes);
+			return new String(bytes, characterEncoding);
+		}
+	}
 
-    /**
-     * 请求参数流
-     *
-     * @param request 请求
-     * @return 返回 请求参数流
-     * @throws IOException 读取流异常
-     * @see HttpServletRequest#getContentLength()  无需关注是否为负数
-     */
-    public static String copyInputStream(HttpServletRequest request) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        IOUtils.copy(request.getInputStream(), byteArrayOutputStream);
-        return byteArrayOutputStream.toString();
-    }
+	/**
+	 * 请求参数流
+	 * @param request 请求
+	 * @return 返回 请求参数流
+	 * @throws IOException 读取流异常
+	 * @see HttpServletRequest#getContentLength() 无需关注是否为负数
+	 */
+	public static String copyInputStream(HttpServletRequest request) throws IOException {
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		IOUtils.copy(request.getInputStream(), byteArrayOutputStream);
+		return byteArrayOutputStream.toString();
+	}
 
 }
