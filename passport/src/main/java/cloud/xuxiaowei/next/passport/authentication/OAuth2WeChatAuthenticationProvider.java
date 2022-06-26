dd4695a2764a46d76602be87fae1cb12ca0964b7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-26 23:11:15 +0800

    暂存

--- passport/src/main/java/cloud/xuxiaowei/next/passport/authentication/OAuth2WeChatAuthenticationProvider.java
+++ passport/src/main/java/cloud/xuxiaowei/next/passport/authentication/OAuth2WeChatAuthenticationProvider.java
@@ -3,15 +3,29 @@ package cloud.xuxiaowei.next.passport.authentication;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AnonymousAuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.*;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.authentication.*;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.context.ProviderContextHolder;
 import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationProvider;
+import org.springframework.security.oauth2.server.authorization.token.DefaultOAuth2TokenContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
+
+import java.security.Principal;
+import java.util.*;
+
+import static cloud.xuxiaowei.next.passport.authentication.OAuth2WeChatAuthenticationToken.WECHAT;
 
 /**
  * 微信 OAuth2 身份验证提供程序
@@ -62,10 +76,90 @@ public class OAuth2WeChatAuthenticationProvider implements AuthenticationProvide
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		OAuth2WeChatAuthenticationToken oauth2WeChatAuthenticationToken = (OAuth2WeChatAuthenticationToken) authentication;
 
-		log.error("暂未完成开发");
+		OAuth2ClientAuthenticationToken clientPrincipal = OAuth2ProviderUtils
+				.getAuthenticatedClientElseThrowInvalidClient(oauth2WeChatAuthenticationToken);
+		RegisteredClient registeredClient = clientPrincipal.getRegisteredClient();
 
-		return null;
+		OAuth2Authorization.Builder builder = OAuth2Authorization.withRegisteredClient(registeredClient);
+		builder.principalName(oauth2WeChatAuthenticationToken.getOpenid());
+		builder.authorizationGrantType(WECHAT);
+
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority("wechat");
+		authorities.add(authority);
+		User user = new User(oauth2WeChatAuthenticationToken.getOpenid(),
+				oauth2WeChatAuthenticationToken.getAdditionalParameters().get("code") + "", authorities);
+		Object details = oauth2WeChatAuthenticationToken.getDetails();
+		UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+				user, details);
+
+		builder.attribute(Principal.class.getName(), usernamePasswordAuthenticationToken);
+		Set<String> scopes = new HashSet<>();
+		scopes.add("snsapi_base");
+		builder.attribute(OAuth2Authorization.AUTHORIZED_SCOPE_ATTRIBUTE_NAME, scopes);
+
+		OAuth2Authorization authorization = builder.build();
+
+		// @formatter:off
+		DefaultOAuth2TokenContext.Builder tokenContextBuilder = DefaultOAuth2TokenContext.builder()
+				.registeredClient(registeredClient)
+				.principal(authorization.getAttribute(Principal.class.getName()))
+				.providerContext(ProviderContextHolder.getProviderContext())
+				.authorization(authorization)
+				.authorizedScopes(authorization.getAttribute(OAuth2Authorization.AUTHORIZED_SCOPE_ATTRIBUTE_NAME))
+				.authorizationGrantType(WECHAT)
+				.authorizationGrant(oauth2WeChatAuthenticationToken);
+		// @formatter:on
+
+		OAuth2Authorization.Builder authorizationBuilder = OAuth2Authorization.from(authorization);
+
+		// ----- Access token -----
+		OAuth2TokenContext tokenContext = tokenContextBuilder.tokenType(OAuth2TokenType.ACCESS_TOKEN).build();
+		OAuth2Token generatedAccessToken = this.tokenGenerator.generate(tokenContext);
+		if (generatedAccessToken == null) {
+			OAuth2Error error = new OAuth2Error(OAuth2ErrorCodes.SERVER_ERROR,
+					"The token generator failed to generate the access token.", ERROR_URI);
+			throw new OAuth2AuthenticationException(error);
+		}
+		OAuth2AccessToken accessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER,
+				generatedAccessToken.getTokenValue(), generatedAccessToken.getIssuedAt(),
+				generatedAccessToken.getExpiresAt(), tokenContext.getAuthorizedScopes());
+		if (generatedAccessToken instanceof ClaimAccessor) {
+			authorizationBuilder.token(accessToken,
+					(metadata) -> metadata.put(OAuth2Authorization.Token.CLAIMS_METADATA_NAME,
+							((ClaimAccessor) generatedAccessToken).getClaims()));
+		}
+		else {
+			authorizationBuilder.accessToken(accessToken);
+		}
+
+		// ----- Refresh token -----
+		OAuth2RefreshToken refreshToken = null;
+		if (registeredClient.getAuthorizationGrantTypes().contains(AuthorizationGrantType.REFRESH_TOKEN) &&
+		// Do not issue refresh token to public client
+				!clientPrincipal.getClientAuthenticationMethod().equals(ClientAuthenticationMethod.NONE)) {
+
+			tokenContext = tokenContextBuilder.tokenType(OAuth2TokenType.REFRESH_TOKEN).build();
+			OAuth2Token generatedRefreshToken = this.tokenGenerator.generate(tokenContext);
+			if (!(generatedRefreshToken instanceof OAuth2RefreshToken)) {
+				OAuth2Error error = new OAuth2Error(OAuth2ErrorCodes.SERVER_ERROR,
+						"The token generator failed to generate the refresh token.", ERROR_URI);
+				throw new OAuth2AuthenticationException(error);
+			}
+			refreshToken = (OAuth2RefreshToken) generatedRefreshToken;
+			authorizationBuilder.refreshToken(refreshToken);
+		}
+
+		authorization = authorizationBuilder.build();
+
+		this.authorizationService.save(authorization);
+
+		Map<String, Object> additionalParameters = Collections.emptyMap();
+
+		return new OAuth2AccessTokenAuthenticationToken(registeredClient, clientPrincipal, accessToken, refreshToken,
+				additionalParameters);
 	}
 
 	@Override
