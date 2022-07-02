605bf838cb1448e8b4b4def4e88b726639b3bcab
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-07-02 17:51:58 +0800

    :tada: 新增WebService服务

---
+++ resource-services-parent/webservice/src/test/java/cloud/xuxiaowei/next/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.next.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
