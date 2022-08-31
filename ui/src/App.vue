35864a8eb158f73365e203abc8c35e4ec1455369
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-08-31 14:28:05 +0800

    :sparkles: 全局样式放入到 style.css

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -24,10 +24,5 @@ router.isReady().then(() => {
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+
 </style>
