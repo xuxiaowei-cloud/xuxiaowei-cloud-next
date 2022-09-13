c04d8c79e5da458fa2107aad65ff3cd219d2179c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-09-13 13:24:46 +0800

    :bug: 补全日志过滤器中的MDC中的值

--- cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/next/system/filter/LogHttpFilter.java
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/next/system/filter/LogHttpFilter.java
@@ -3,6 +3,7 @@ package cloud.xuxiaowei.next.system.filter;
 import cloud.xuxiaowei.next.log.service.ILogService;
 import cloud.xuxiaowei.next.utils.Constant;
 import cloud.xuxiaowei.next.utils.RequestUtils;
+import cloud.xuxiaowei.next.utils.SecurityUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
@@ -12,9 +13,12 @@ import jakarta.servlet.http.HttpSession;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
@@ -47,6 +51,20 @@ public class LogHttpFilter extends HttpFilter {
 		String remoteHost = req.getRemoteHost();
 		MDC.put(Constant.IP, remoteHost);
 
+		String authorization = RequestUtils.getAuthorization(req);
+		Map<String, String> payloadMap = SecurityUtils.getPayloadStringMap(authorization);
+		String usersId = payloadMap.get(Constant.USERS_ID);
+		MDC.put(Constant.USERS_ID, usersId);
+
+		String username = payloadMap.get(Constant.USERNAME);
+		String sub = payloadMap.get(JwtClaimNames.SUB);
+		if (StringUtils.hasText(username)) {
+			MDC.put(Constant.NAME, username);
+		}
+		else if (StringUtils.hasText(sub)) {
+			MDC.put(Constant.NAME, sub);
+		}
+
 		String requestId = req.getHeader(Constant.REQUEST_ID);
 		if (requestId == null) {
 			requestId = UUID.randomUUID().toString();
@@ -63,7 +81,6 @@ public class LogHttpFilter extends HttpFilter {
 		String requestUri = req.getRequestURI();
 		String queryString = req.getQueryString();
 		String headersMap = RequestUtils.getHeadersJson(req);
-		String authorization = RequestUtils.getAuthorization(req);
 		String userAgent = RequestUtils.getUserAgent(req);
 
 		logService.saveLog(remoteHost, requestId, sessionId, method, requestUri, queryString, headersMap, authorization,
