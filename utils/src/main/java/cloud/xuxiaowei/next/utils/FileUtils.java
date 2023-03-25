c1a0087b13bf86c38d9598d12c98692daf7c4de8
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-03-25 20:28:11 +0800

    :bug: 文件工具类

--- utils/src/main/java/cloud/xuxiaowei/next/utils/FileUtils.java
+++ utils/src/main/java/cloud/xuxiaowei/next/utils/FileUtils.java
@@ -3,9 +3,9 @@ package cloud.xuxiaowei.next.utils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -21,74 +21,65 @@ public class FileUtils {
 
 	/**
 	 * 根据 URL 路径 下载文件
-	 * @param uri URL
+	 * @param urlPath URL
 	 * @param filePath 路径
 	 * @return 文件大小
 	 */
-	public static int downloadFile(String uri, String filePath) {
-
-		URL url;
+	public static int downloadFile(String urlPath, String filePath) {
+		HttpURLConnection conn = null;
+		InputStream inputStream = null;
+		FileOutputStream outputStream = null;
 		try {
-			url = new URL(uri);
+			URL url = new URL(urlPath);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setConnectTimeout(5 * 1000);
+			conn.connect();
+			if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				inputStream = conn.getInputStream();
+				outputStream = new FileOutputStream(filePath);
+				byte[] buffer = new byte[1024];
+				int length;
+				while ((length = inputStream.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, length);
+				}
+				log.info("URL：{} 文件：{} 大小：{}", urlPath, filePath, conn.getContentLength());
+				return conn.getContentLength();
+			}
+			else {
+				log.error("下载 URL：{} 失败，状态码：{}", urlPath, conn.getResponseCode());
+				return 0;
+			}
 		}
 		catch (MalformedURLException e) {
-			log.error("创建下载 URL：{} 异常：", uri, e);
+			log.error("创建下载 URL：{} 异常：", urlPath, e);
 			throw new RuntimeException(e);
-		}
-
-		URLConnection urlConnection;
-		try {
-			urlConnection = url.openConnection();
 		}
 		catch (IOException e) {
-			log.error("打开 URL：{} 链接异常：", uri, e);
+			log.error("打开 URL：{} 链接异常：", urlPath, e);
 			throw new RuntimeException(e);
 		}
-
-		InputStream inputStream;
-		try {
-			inputStream = urlConnection.getInputStream();
-		}
-		catch (IOException e) {
-			log.error("获取 URL：{} 输入流异常：", uri, e);
-			throw new RuntimeException(e);
-		}
-
-		FileOutputStream fileOutputStream;
-		try {
-			fileOutputStream = new FileOutputStream(filePath);
-		}
-		catch (FileNotFoundException e) {
-			log.error("URL：{} 文件：{} 输出流异常：", uri, filePath, e);
-			throw new RuntimeException(e);
-		}
-
-		byte[] buffer = new byte[1204];
-		int byteSum = 0;
-		int byteRead;
-		while (true) {
-			try {
-				if ((byteRead = inputStream.read(buffer)) == -1) {
-					break;
+		finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				}
+				catch (IOException e) {
+					log.error("关闭 inputStream 异常：", e);
 				}
 			}
-			catch (IOException e) {
-				log.error("URL：{} 文件：{} 读取异常：", uri, filePath, e);
-				throw new RuntimeException(e);
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				}
+				catch (IOException e) {
+					log.error("关闭 outputStream 异常：", e);
+				}
 			}
-			byteSum += byteRead;
-			try {
-				fileOutputStream.write(buffer, 0, byteRead);
-			}
-			catch (IOException e) {
-				log.error("URL：{} 文件：{} 写入异常：", uri, filePath, e);
-				throw new RuntimeException(e);
+			if (conn != null) {
+				conn.disconnect();
 			}
 		}
-
-		log.info("URL：{} 文件：{} 大小：{}", uri, filePath, byteSum);
-
-		return byteSum;
 	}
 
 	/**
@@ -111,42 +102,50 @@ public class FileUtils {
 					name = entry.getName();
 					name = name.substring(0, name.length() - 1);
 					File fileObject = new File(zipDir + File.separator + name);
-					boolean mkdir = fileObject.mkdir();
+					if (!fileObject.exists()) {
+						boolean mkdir = fileObject.mkdirs();
+						if (!mkdir) {
+							log.error("创建解压目录 {} 失败！", fileObject.getAbsolutePath());
+							throw new RuntimeException("创建解压目录失败！");
+						}
+					}
 				}
 			}
 
-			int buffer = 1024;
+			int bufferSize = 1024;
 
 			Enumeration<? extends ZipEntry> e = zipfile.entries();
 			while (e.hasMoreElements()) {
 				entry = e.nextElement();
 				if (entry.isDirectory()) {
+					continue;
+				}
+				String entryName = entry.getName();
+				if ("".equals(entryName.trim())) {
+					log.warn("entryName is null or empty, entry={}", entry);
+					continue;
+				}
+				try (InputStream inputStream = zipfile.getInputStream(entry);
+						OutputStream outputStream = new FileOutputStream(zipDir + File.separator + entryName);
+						BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+						BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream,
+								bufferSize)) {
+
+					int count;
+					byte[] dataByte = new byte[bufferSize];
+
+					while ((count = bufferedInputStream.read(dataByte, 0, bufferSize)) != -1) {
+						bufferedOutputStream.write(dataByte, 0, count);
+					}
 
 				}
-				else {
-					try (BufferedInputStream bufferedInputStream = new BufferedInputStream(
-							zipfile.getInputStream(entry));
-							FileOutputStream fileOutputStream = new FileOutputStream(
-									zipDir + File.separator + entry.getName());
-							BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream,
-									buffer)) {
-
-						int count;
-						byte[] dataByte = new byte[buffer];
-
-						while ((count = bufferedInputStream.read(dataByte, 0, buffer)) != -1) {
-							bufferedOutputStream.write(dataByte, 0, count);
-						}
-
-					}
-					catch (FileNotFoundException ex) {
-						log.error("文件不存在异常：{}", filePath, ex);
-						throw new RuntimeException(ex);
-					}
-					catch (IOException ex) {
-						log.error("解压异常：{}", filePath, ex);
-						throw new RuntimeException(ex);
-					}
+				catch (FileNotFoundException ex) {
+					log.error("文件不存在异常：{}", filePath, ex);
+					throw new RuntimeException(ex);
+				}
+				catch (IOException ex) {
+					log.error("解压异常：{}", filePath, ex);
+					throw new RuntimeException(ex);
 				}
 			}
 
